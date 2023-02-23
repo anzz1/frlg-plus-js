23ca94a338111242deb485e0fb27cb2a15c3ae7a
anzz1 <anzz1@live.com>
2023-02-23 05:31:41 +0200

    Add files via upload

--- js/worker_apply.js
+++ js/worker_apply.js
@@ -3,14 +3,14 @@
 self.importScripts(
 	'./MarcFile.js',
 	'./crc.js',
-	'./formats/ips.js',
-	'./formats/aps.js',
-	'./formats/ups.js',
-	'./formats/bps.js',
-	'./formats/rup.js',
-	'./formats/ppf.js',
-	'./formats/pmsr.js',
-	'./formats/vcdiff.js'
+//	'./formats/ips.js',
+//	'./formats/aps.js',
+//	'./formats/ups.js',
+	'./formats/bps.js'
+//	'./formats/rup.js',
+//	'./formats/ppf.js',
+//	'./formats/pmsr.js',
+//	'./formats/vcdiff.js'
 );
 
 
@@ -22,22 +22,28 @@ self.onmessage = event => { // listen for messages from the main thread
 
 	var patch;
 	var header=patchFile.readString(6);
-	if(header.startsWith(IPS_MAGIC)){
-		patch=parseIPSFile(patchFile);
-	}else if(header.startsWith(UPS_MAGIC)){
-		patch=parseUPSFile(patchFile);
-	}else if(header.startsWith(APS_MAGIC)){
-		patch=parseAPSFile(patchFile);
-	}else if(header.startsWith(BPS_MAGIC)){
+//	if(header.startsWith(IPS_MAGIC)){
+//		patch=parseIPSFile(patchFile);
+//	}else if(header.startsWith(UPS_MAGIC)){
+//		patch=parseUPSFile(patchFile);
+//	}else if(header.startsWith(APS_MAGIC)){
+//		patch=parseAPSFile(patchFile);
+//	}else if(header.startsWith(BPS_MAGIC)){
+//		patch=parseBPSFile(patchFile);
+//	}else if(header.startsWith(RUP_MAGIC)){
+//		patch=parseRUPFile(patchFile);
+//	}else if(header.startsWith(PPF_MAGIC)){
+//		patch=parsePPFFile(patchFile);
+//	}else if(header.startsWith(PMSR_MAGIC)){
+//		patch=parseMODFile(patchFile);
+//	}else if(header.startsWith(VCDIFF_MAGIC)){
+//		patch=parseVCDIFF(patchFile);
+//	}else{
+//		errorMessage='error_invalid_patch';
+//	}
+
+	if(header.startsWith(BPS_MAGIC)){
 		patch=parseBPSFile(patchFile);
-	}else if(header.startsWith(RUP_MAGIC)){
-		patch=parseRUPFile(patchFile);
-	}else if(header.startsWith(PPF_MAGIC)){
-		patch=parsePPFFile(patchFile);
-	}else if(header.startsWith(PMSR_MAGIC)){
-		patch=parseMODFile(patchFile);
-	}else if(header.startsWith(VCDIFF_MAGIC)){
-		patch=parseVCDIFF(patchFile);
 	}else{
 		errorMessage='error_invalid_patch';
 	}
